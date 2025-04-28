f6126f8efae59e59004dadf86358a5691f03b354
Charlotte Goh Xiao Ton <xiaoton.bestweb@gmail.com>
2025-04-28 23:52:02 +0800

    test

--- src/OWSB/OWSB.java
+++ src/OWSB/OWSB.java
@@ -17,6 +17,7 @@ public class OWSB {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
         FinanceManagerPanel fmg = new FinanceManagerPanel();
         fmg.setVisible(true);
     }
